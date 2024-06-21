31ab17f110bca89e691038815cae3cedd02763b9
John <john-romagno@outlook.com>
2024-06-21 11:57:19 -0400

    work save

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../../db');
+const pool = require('../db');
 
 // Register a new user
 router.post('/register', async (req, res) => {
